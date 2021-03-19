14c5c8e8ee66771e06f34cec74a94c7d09886f07
Andriy Savytskyy <andy1978dnepr@gmail.com>
2021-03-19 22:20:16 +0200

    done some cleanup

--- src/main/java/org/example/notesapp/servlets/ViewAddNotesServlet.java
+++ src/main/java/org/example/notesapp/servlets/ViewAddNotesServlet.java
@@ -4,14 +4,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.notesapp.dto.NoteInput;
-import org.example.notesapp.dto.NoteOutput;
 import org.example.notesapp.entities.Note;
 import org.example.notesapp.dto.NotesResponse;
 import org.example.notesapp.services.NotesRepository;
 import org.example.notesapp.services.UberFactory;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,17 +19,7 @@ public class ViewAddNotesServlet extends JsonServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        //List<Note> notes = repository.getAll();
 
-
-//        return response.getContacts().stream().map(c -> {
-//            Contact contact = new Contact();
-//            contact.setId(c.getId());
-//            contact.setName(c.getName());
-//            contact.setType(Contact.ContactType.valueOf(c.getType().toUpperCase()));
-//            contact.setValue(c.getValue());
-//            return contact;
-//        }).collect(Collectors.toList());
 
         List<NotesResponse.Note> response = repository.getAll().stream().map(n -> {
             return new NotesResponse.Note()
